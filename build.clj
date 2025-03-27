cedde2b32658e184b896447a8a054d9d250ebad7
jaide <jayzawrotny@gmail.com>
2025-03-27 14:19:00 -0400

    Update deploy config

--- build.clj
+++ build.clj
@@ -4,12 +4,11 @@
 
 (def project (-> (edn/read-string (slurp "deps.edn"))
                  :aliases :neil :project))
-(def lib (or (:name project) 'my/lib1))
+(def lib (assert (:name project) "Missing project name"))
 
 ;; use neil project set version 1.2.0 to update the version in deps.edn
 
-(def version (or (:version project)
-                 "1.2.0"))
+(def version (assert (:version project) "Missing project version"))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
@@ -39,7 +38,7 @@
 
 (defn uber [_]
   (clean nil)
-  (b/copy-dir {:src-dirs ["src" "resources"]
+  (b/copy-dir {:src-dirs ["src/main"]
                :target-dir class-dir})
   (b/compile-clj {:basis basis
                   :src-dirs ["src"]
